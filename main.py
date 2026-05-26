6ce40788ddb7243874a11c0965f93f7767d62211
mintcoco1203 <dg2026517@danggok.hs.kr>
2026-05-26 11:58:07 +0900

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,14 @@ boxobj2 = box(pos=vec(0,0,0),size=vec(0.5,0.1,0.5),color=color.green)
 boxobj3 = box(pos=vec(0.7,0,0),size=vec(0.5,0.1,0.5),color=color.green)
 boxobj4 = box(pos=vec(-0.35,0,-0.5),size=vec(0.5,0.1,0.5),color=color.green)
 boxobj5 = box(pos=vec(0.35,0,-0.5),size=vec(0.5,0.1,0.5),color=color.green)
+boxobj6 = box(pos=vec(0.4,0,0.5),size=vec(0.4,0.1,0.4),color=color.green)
+boxobj7 = box(pos=vec(-0.35,0,0.5),size=vec(0.4,0.1,0.4),color=color.green)
+boxobj8 = box(pos=vec(-0.9,0,0.3),size=vec(0.4,0.1,0.4),color=color.green)
+boxobj9 = box(pos=vec(0.9,0,0.5),size=vec(0.4,0.1,0.4),color=color.green)
+boxobj10 = box(pos=vec(1.1,0,0.7),size=vec(0.4,0.1,0.4),color=color.green)
+boxobj11 = box(pos=vec(-1.1,0,0.8),size=vec(0.4,0.1,0.4),color=color.green)
+boxobj12 = box(pos=vec(0.5,0,1),size=vec(0.4,0.1,0.4),color=color.green)
+boxobj13 = box(pos=vec(-0.5,0,1),size=vec(0.4,0.1,0.4),color=color.green)
     
 level = 1
 maxLevel = 11
@@ -12,6 +20,15 @@ def updateBox():
     boxobj2.size=vec(0.5, level*1.25, 0.5)
     boxobj3.size=vec(0.5, level*1.25, 0.5)
     boxobj4.size=vec(0.5, level*1.25,0.5)
+    boxobj5.size=vec(0.4, level*1.26,0.4)
+    boxobj6.size=vec(0.4, level*1.23,0.4)
+    boxobj7.size=vec(0.4, level*1.1,0.4)
+    boxobj8.size=vec(0.4, level*1.22,0.4)
+    boxobj9.size=vec(0.4, level*1.2,0.4)
+    boxobj10.size=vec(0.4, level*1.3,0.4)
+    boxobj11.size=vec(0.4, level*1.05,0.4)
+    boxobj12.size=vec(0.4, level*1.1,0.4)
+    boxobj13.size=vec(0.4, level*1.3,0.4)
 
 def keyInput(evt):
     global level
