83c131c133482c59bbb30ddeb089ee385b333618
mintcoco1203 <dg2026517@danggok.hs.kr>
2026-06-05 11:35:38 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
-my_cylinder = cylinder(pos=vec(0, -2, 0),axis=vec(0, 2, 0),radius=1,color=color.red)
-spout = cylinder(pos=vec(0.5, -1.7, 0),axis=vec(2.7, 1, 0),radius=0.3,color=color.red)
 handle = ring(pos=vec(-0.1, -0.2, 0),axis=vec(0, 0, 1),radius=1.5,thickness=0.2,color=color.red)
-wateringcan= compound( [ my_cylinder, spout ,handle])
-wateringcan.axis = vec(1,-0.8,-0.3)
-boxobj1 = box(pos=vec(-0.7,0,0),size=vec(0.5,0.1,0.5),color=color.green)
-boxobj2 = box(pos=vec(0,0,0),size=vec(0.5,0.1,0.5),color=color.green)
-boxobj3 = box(pos=vec(0.7,0,0),size=vec(0.5,0.1,0.5),color=color.green)
-boxobj4 = box(pos=vec(-0.35,0,-0.5),size=vec(0.5,0.1,0.5),color=color.green)
-boxobj5 = box(pos=vec(0.35,0,-0.5),size=vec(0.5,0.1,0.5),color=color.green)
+wateringcan= compound([ my_cylinder, spout ,handle])
+wateringcan.axis = vec(1,-0.6,0)
+wateringcan.pos = vec(-6,5,0)
+box(size=vec(100,0.01,100),color=color.yellow)
+boxobj1 = box(pos=vec(-0.7,0,0),size=vec(0.4,0.1,0.4),color=color.green)
+boxobj2 = box(pos=vec(0,0,0),size=vec(0.4,0.1,0.4),color=color.green)
+boxobj3 = box(pos=vec(0.7,0,0),size=vec(0.4,0.1,0.4),color=color.green)
+boxobj4 = box(pos=vec(-0.35,0,-0.4),size=vec(0.4,0.1,0.4),color=color.green)
+boxobj5 = box(pos=vec(0.35,0,-0.5),size=vec(0.4,0.1,0.4),color=color.green)
 boxobj6 = box(pos=vec(0.4,0,0.5),size=vec(0.4,0.1,0.4),color=color.green)
 boxobj7 = box(pos=vec(-0.35,0,0.5),size=vec(0.4,0.1,0.4),color=color.green)
 boxobj8 = box(pos=vec(-0.9,0,0.3),size=vec(0.4,0.1,0.4),color=color.green)
@@ -16,14 +16,22 @@ boxobj10 = box(pos=vec(1.1,0,0.7),size=vec(0.4,0.1,0.4),color=color.green)
 boxobj11 = box(pos=vec(-1.1,0,0.8),size=vec(0.4,0.1,0.4),color=color.green)
 boxobj12 = box(pos=vec(0.5,0,1),size=vec(0.4,0.1,0.4),color=color.green)
 boxobj13 = box(pos=vec(-0.5,0,1),size=vec(0.4,0.1,0.4),color=color.green)
+
+
+
+
     
-level = 1
+level = 0
 maxLevel = 11
+
+state = ["1level","2level","3level","4level","5level","6level","7level","8level","9level","10level"]
+
 
 while True :
     rate(3)
     k = keysdown()
     if '1' in k:
+        print(state[level])
         if level < maxLevel:
             level += 1
             boxobj1.size=vec(0.5, level*1.25, 0.5)
@@ -39,21 +47,21 @@ while True :
             boxobj11.size=vec(0.4, level*1.05,0.4)
             boxobj12.size=vec(0.4, level*1.1,0.4)
             boxobj13.size=vec(0.4, level*1.3,0.4)
+            if level == maxLevel:
+                print("다 자랐다!")
     if ' ' in k:
-        boxobj1 = box(pos=vec(-0.7,0,0),size=vec(0.4,0.1,0.4))
-        boxobj2 = box(pos=vec(0,0,0),size=vec(0.4,0.1,0.4))
-        boxobj3 = box(pos=vec(0.7,0,0),size=vec(0.4,0.1,0.4))
-        boxobj4 = box(pos=vec(-0.35,0,-0.4),size=vec(0.4,0.1,0.4))
-        boxobj5 = box(pos=vec(0.35,0,-0.5),size=vec(0.4,0.1,0.4))
-        boxobj6 = box(pos=vec(0.4,0,0.5),size=vec(0.4,0.1,0.4))
-        boxobj7 = box(pos=vec(-0.35,0,0.5),size=vec(0.4,0.1,0.4))
-        boxobj8 = box(pos=vec(-0.9,0,0.3),size=vec(0.4,0.1,0.4))
-        boxobj9 = box(pos=vec(0.9,0,0.5),size=vec(0.4,0.1,0.4))
-        boxobj10 = box(pos=vec(1.1,0,0.7),size=vec(0.4,0.1,0.4))
-        boxobj11 = box(pos=vec(-1.1,0,0.8),size=vec(0.4,0.1,0.4))
-        boxobj12 = box(pos=vec(0.5,0,1),size=vec(0.4,0.1,0.4))
-        boxobj13 = box(pos=vec(-0.5,0,1),size=vec(0.4,0.1,0.4))
-while True : 
-    rate(100)
-    if level == 11:
-        print("다 자랐다!")
+        level=0
+        boxobj1.size = vec(0.4,0.1,0.4)
+        boxobj2.size = vec(0.4,0.1,0.4)
+        boxobj3.size = vec(0.4,0.1,0.4)
+        boxobj4.size = vec(0.4,0.1,0.4)
+        boxobj5.size = vec(0.4,0.1,0.4)
+        boxobj6.size = vec(0.4,0.1,0.4)
+        boxobj7.size = vec(0.4,0.1,0.4)
+        boxobj8.size = vec(0.4,0.1,0.4)
+        boxobj9.size = vec(0.4,0.1,0.4)
+        boxobj10.size = vec(0.4,0.1,0.4)
+        boxobj11.size = vec(0.4,0.1,0.4)
+        boxobj12.size = vec(0.4,0.1,0.4)
+        boxobj13.size = vec(0.4,0.1,0.4)
+        print("초기화")
